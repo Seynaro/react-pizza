37cf50aa77bdbb0e5137048c57c59b06114c1a6f
Khamzat <xamzatseynaro@gmail.com>
2020-10-11 22:46:16 +0300

    map Categories

---
+++ src/components/Categories.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+function Categories({ items, onClickItem }) {
+    return (
+        <div className="categories">
+            <ul>
+                <li className="active">Все</li>
+                {
+                    items.map((name, index) => {
+                        return <li onClick={() => onClickItem(name)} key={`${name}_${index}`}>{name}</li>
+                    })
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default Categories
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import logoSvg from "./assets/img/pizza-logo.svg";
+import logoSvg from "../assets/img/pizza-logo.svg";
 import React from "react";
 import Button from "./Button";
 
