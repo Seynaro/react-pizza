d692dba6dc89cb49e4d3310dbc35f745513ad051
Khamzat <xamzatseynaro@gmail.com>
2020-10-12 23:13:28 +0300

    add useState for button-'active'

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 
 function Categories({ items, onClickItem }) {
+
+    const [activeItem, setActiveItem] = useState(null)
+    const onSelectItem = (index) => {
+        setActiveItem(index)
+    }
+
     return (
         <div className="categories">
             <ul>
-                <li className="active">Все</li>
+                <li className={activeItem === null ? 'active' : ''}
+                    onClick={() => onSelectItem(null)}>Все</li>
                 {
                     items.map((name, index) => {
-                        return <li onClick={() => onClickItem(name)} key={`${name}_${index}`}>{name}</li>
+                        return <li className={activeItem === index ? 'active' : ''}
+                            onClick={() => onSelectItem(index)} key={`${name}_${index}`}>{name}</li>
                     })
                 }
             </ul>
