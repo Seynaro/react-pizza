c6153ff0850145e9c76640a07109e4d2cbd2043f
Khamzat <xamzatseynaro@gmail.com>
2020-10-18 21:44:30 +0300

    change fetch to axios

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './scss/app.scss';
 import {Header} from "./components";
 import {Home, Cart} from "./pages";
 import {Route} from "react-router-dom";
+import axios from 'axios';
 
 
 function App() {
@@ -10,12 +11,11 @@ function App() {
     const [pizzas, setPizzas] = React.useState([]);
 
     React.useEffect(() => {
-        fetch('http://localhost:3000/db.json')
-            .then((resp) => resp.json())
-            .then(json => {
-                setPizzas(json.pizzas)
+        axios.get('http://localhost:3000/db.json')
+            .then(({ data }) => {
+                setPizzas(data.pizzas)
             })
-    }, []);
+            }, []);
 
     return (
         <div className="wrapper">
