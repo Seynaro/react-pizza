2a5d838159e689e35d5789146782354ec17b1eb1
Khamzat <xamzatseynaro@gmail.com>
2020-10-20 22:47:16 +0300

    add filters

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App";
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import './scss/app.scss';
+import {Provider} from "react-redux";
+import store from './redux/store';
+
 
 ReactDOM.render(
   <React.StrictMode>
-      <BrowserRouter>
+      <Router>
+          <Provider store={store}>
       <App />
-      </BrowserRouter>
+          </Provider>
+      </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
