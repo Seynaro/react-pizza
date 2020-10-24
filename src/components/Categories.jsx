cfe8a02c6944a83db5e17b5ecea9fdf6ff093449
Khamzat <xamzatseynaro@gmail.com>
2020-10-24 23:37:01 +0300

    refactoring Categories, Home

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 
-function Categories({items}) {
+const Categories = React.memo(function Categories({items, onClickItem}) {
 
     const [activeItem, setActiveItem] = React.useState(null)
     const onSelectItem = (index) => {
-        setActiveItem(index)
+        setActiveItem(index);
+        onClickItem(index)
     }
 
     return (
@@ -23,6 +24,6 @@ function Categories({items}) {
             </ul>
         </div>
     )
-}
+});
 
 export default Categories
