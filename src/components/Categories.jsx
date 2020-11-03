eebae44e60e89da2c50634269d7819327f45b4dc
Khamzat <xamzatseynaro@gmail.com>
2020-11-03 21:23:13 +0300

    add sorting

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,29 +1,35 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-
-const Categories = React.memo(function Categories({items, onClickItem}) {
-
-    const [activeItem, setActiveItem] = React.useState(null)
-    const onSelectItem = (index) => {
-        setActiveItem(index);
-        onClickItem(index)
-    }
-
-    return (
-        <div className="categories">
-            <ul>
-                <li className={activeItem === null ? 'active' : ''}
-                    onClick={() => onSelectItem(null)}>Все
-                </li>
-                {
-                    items && items.map((name, index) => (
-                        <li className={activeItem === index ? 'active' : ''}
-                            onClick={() => onSelectItem(index)} key={`${name}_${index}`}>{name}</li>
-                    ))
-                }
-            </ul>
-        </div>
-    )
+const Categories = React.memo(function Categories({ activeCategory, items, onClickCategory }) {
+  return (
+    <div className="categories">
+      <ul>
+        <li
+          className={activeCategory === null ? 'active' : ''}
+          onClick={() => onClickCategory(null)}>
+          Все
+        </li>
+        {items &&
+          items.map((name, index) => (
+            <li
+              className={activeCategory === index ? 'active' : ''}
+              onClick={() => onClickCategory(index)}
+              key={`${name}_${index}`}>
+              {name}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 });
 
-export default Categories
+Categories.propTypes = {
+  // activeCategory: PropTypes.oneOf([PropTypes.number, null]),
+  items: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onClickCategory: PropTypes.func.isRequired,
+};
+
+Categories.defaultProps = { activeCategory: null, items: [] };
+
+export default Categories;
